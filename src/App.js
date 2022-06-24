a1421042717080935076f9c9e28a7b922e63c889
khush-01 <khushjain2001@gmail.com>
2022-06-25 02:42:11 +0530

    Completed 8-Ball App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useContext } from "react";
+import { MyContext } from "./context";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
+import "./App.css";
+import "animate.css";
+import Initial from "./components/Initial";
+import Confirm from "./components/Confirm";
+import Result from "./components/Result";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
+
+const App = () => {
+  const context = useContext(MyContext);
+
+  const handleComponent = () => {
+    let screen = context.state.screen;
+    if (screen === 0) return <Initial />;
+    if (screen === 1) return <Confirm />;
+    if (screen === 2) return <Result />;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <SwitchTransition mode="out-in">
+          <CSSTransition
+            key={context.state.screen}
+            timeout={400}
+            classNames="zoom"
+          >
+            {handleComponent()}
+          </CSSTransition>
+        </SwitchTransition>
+      </div>
+
+      <ToastContainer position="top-left" autoClose={3000} />
+
+      <footer>Copyright &copy; {new Date().getFullYear()}</footer>
     </div>
   );
-}
+};
 
 export default App;
